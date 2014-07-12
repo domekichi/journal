9ec258faeba74ecbc91ce6fb67f1cd47292e90ed
TOKUDOME Shohei <domekichi@gmail.com>
2014-07-13 06:34:51 +0900

    first commit

---
+++ fuel/app/config/development/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The development database settings. These get merged with the global settings.
+ */
+
+return array(
+	'default' => array(
+        'connection' => array(
+            'dsn' => 'sqlite:' . APPPATH . 'tmp/journal.sqlite3',
+            'persistent' => false,
+            'compress' => false,
+        ),
+        'identifier' => '"',
+        'table_prefix' => '',
+        'enable_cache' => true,
+        'profiling' => true,
+        'charset' => '',
+    ),
+);
